e376e23b2fbd33d2b9c8448d8afbdddaa3275a13
adsellor <zaven.snkhchyan@gmail.com>
2017-03-21 07:45:54 +0400

    Picture Preview

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -1,11 +1,17 @@
-import {initializeApp} from 'firebase';
+import * as firebase from 'firebase';
 
 import config from '../../config';
 
- firebase.initializeApp({
-    apiKey: config.API_KEY,
-    authDomain: config.AUTH_DOMAIN,
-    databaseURL: config.DATABASE_URL,
-   storageBucket: config.STORAGE_BUCKET,
-    messagingSenderId: config.MESSAGING_SENDER_ID
-  });
+
+export default class Firebase {
+
+static initialise(){
+  firebase.initializeApp({
+      apiKey: config.API_KEY,
+      authDomain: config.AUTH_DOMAIN,
+      databaseURL: config.DATABASE_URL,
+      storageBucket: config.STORAGE_BUCKET,
+      messagingSenderId: config.MESSAGING_SENDER_ID
+    });
+  }
+}
